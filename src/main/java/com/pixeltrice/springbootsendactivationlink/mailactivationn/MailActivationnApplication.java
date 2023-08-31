b0c4ab4848530cc95e8485d79a6742efd5543a6c
User <pavadi.abhishek@gmail.com>
2023-08-30 23:16:22 -0700

    Mail activationlink gmail -Initial commit

---
+++ src/main/java/com/pixeltrice/springbootsendactivationlink/mailactivationn/MailActivationnApplication.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.springbootsendactivationlink.mailactivationn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailActivationnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailActivationnApplication.class, args);
+    }
+
+}
